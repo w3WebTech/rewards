3c03e62875e2d20d1277f84f2d8c3f9c26109ce8
w3WebTech <forw3webtech2@gmail.com>
2024-05-10 13:58:42 +0530

    fix

--- components/MainSection.vue
+++ components/MainSection.vue
@@ -331,14 +331,14 @@ export default defineComponent({
   },
   methods: {
     closerefferalPage() {
-      this?.isReferalRewards = false;
+      this.isReferalRewards = false;
     },
     closependingPage() {
       this.isPendingReferal = false;
     },
     callpage(pageName) {
       if (pageName == "referral_rewards") {
-        this?.isReferalRewards = true;
+        this.isReferalRewards = true;
       }
       if (pageName == "pending_referrals") {
         this.isPendingReferal = true;

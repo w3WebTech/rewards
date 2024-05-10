17c2a2c3c79f73c1e2af7f2c2007dc18cccec107
w3WebTech <forw3webtech2@gmail.com>
2024-05-10 16:26:20 +0530

    fix

--- components/ReferalRewards.vue
+++ components/ReferalRewards.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container mx-auto lg:w-10/12 mt-5  bg-white text-black ">
+  <div class="container mx-auto lg:w-10/12 mt-5 h-[100vh] bg-white text-black ">
     <nav class="navbar navbar-dark bg-dark">
       <div class="flex justify-content-between items-center">
         <button

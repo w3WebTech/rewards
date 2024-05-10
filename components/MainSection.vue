733dcad2d2776314e7dc680daddf9063c24fb50b
w3WebTech <forw3webtech2@gmail.com>
2024-05-10 16:17:08 +0530

    fix

--- components/MainSection.vue
+++ components/MainSection.vue
@@ -479,5 +479,8 @@ export default defineComponent({
   border-bottom-right-radius:0 !important;
   transition: all 450ms cubic-bezier(0.32,1,0.23,1) 0ms !important;
 }
+.container{
+  width:90% !important;
+}
 </style>
 

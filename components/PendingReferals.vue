9928409577a7500bab8355e9fdb097d7367215f4
w3WebTech <forw3webtech2@gmail.com>
2024-05-10 16:29:12 +0530

    fix

--- components/PendingReferals.vue
+++ components/PendingReferals.vue
@@ -1,6 +1,6 @@
 <template>
    <div class="container lg:w-10/12 mx-auto mt-5 h-screen bg-white text-black ">
-  <nav class=" text-black  flex items-center justify-between py-5">
+  <nav class=" text-black  flex items-center justify-between ">
     <div class="flex items-center">
       <button @click="closepage" class="">
        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
